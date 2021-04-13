cb838a2c2f25ef99cd9679cfd7b2b74e8af350d5
Zhaopengcheng <pengcheng57@163.com>
2021-04-13 09:14:09 +0800

    注册处理

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
+
 const express = require('express')
 const router = express.Router()
+
+const md5 = require('blueimp-md5')
+
+const User = require('./models/user')
 
 router.get('/', (req, res) => {
   res.render('index.html')
@@ -18,6 +23,32 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-
+  const { body: { email, nickname } } = req
+  body.password = md5(md5(body.password))
+  User.findOne({
+    $or: [
+      { email },
+      { nickname }
+    ]
+  }).then(data => {
+    if (data) {
+      return res.status(500).json({
+        err_code: 1,
+        message: '邮箱或昵称已存在'
+      })
+    }else{
+      User.create(req.body).then(info => {
+        return res.status(200).json({
+          err_code: 0,
+          message: '注册成功'
+        })
+      })
+    }
+  }).catch(err => {
+    return res.status(500).json({
+      err_code: 500,
+      message: '服务端错误'
+    })
+  })
 })
 module.exports = router
